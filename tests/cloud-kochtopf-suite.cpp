cadeb93db9321b02f24fa7afd994d53ff17d4276
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-12-06 07:44:44 +0100

    cloud topf suite: create fixture

--- tests/cloud-kochtopf-suite.cpp
+++ tests/cloud-kochtopf-suite.cpp
@@ -8,45 +8,78 @@
 using namespace std::chrono_literals;
 
 
-TEST(cloud_kochtopf_suite, basic_usage)
+struct cloud_kochtopf_suite : public ::testing::Test
 {
-    auto sensor = std::make_shared<MockSensor>(27.4);
-    auto switcH = std::make_shared<MockSwitch>(false);
+    void SetUp() override
+    {
+        sensor = std::make_shared<MockSensor>(0.0);
+        switcH = std::make_shared<MockSwitch>(false);
 
-    CloudKochtopf kt(666, sensor, switcH, 1/*milli interval*/);
+        topf = new CloudKochtopf(666, sensor, switcH, 1/*milli interval*/);
 
-    PowerControl* pc = nullptr;
-    TemperatureControl* tc = nullptr;
+        temperature_control = nullptr;
+        power_control = nullptr;
 
-    const DeviceInfo* info = kt.get_info();
-    ASSERT_EQ(info->id(), 666);
-    for (auto [name, type]: info->capabilities()) {
-        switch (type)
-        {
-            case DeviceInfo::POWER_CONTROL: {
-                Control* c = kt.get_control(name);
-                pc = dynamic_cast<PowerControl*>(c);
-                ASSERT_NE(pc, nullptr);
-                break;
-            }
-            case DeviceInfo::TEMPERATURE_CONTROL: {
-                Control* c = kt.get_control(name);
-                tc = dynamic_cast<TemperatureControl*>(c);
-                ASSERT_NE(tc, nullptr);
-                break;
+        const DeviceInfo* info = topf->get_info();
+        ASSERT_EQ(info->id(), 666);
+        for (auto [name, type]: info->capabilities()) {
+            switch (type)
+            {
+                case DeviceInfo::POWER_CONTROL: {
+                    Control* c = topf->get_control(name);
+                    power_control = dynamic_cast<PowerControl*>(c);
+                    ASSERT_NE(power_control, nullptr);
+                    break;
+                }
+                case DeviceInfo::TEMPERATURE_CONTROL: {
+                    Control* c = topf->get_control(name);
+                    temperature_control = dynamic_cast<TemperatureControl*>(c);
+                    ASSERT_NE(temperature_control, nullptr);
+                    break;
+                }
             }
         }
+
+        power_control->start();
     }
 
-    tc->set_target_temperature(91);
-    pc->start();
+    void TearDown() override
+    {
+        power_control->stop();
+    }
 
-    sensor->set_temperature(95);
+    CloudDevice* topf;
+    std::shared_ptr<MockSensor> sensor;
+    std::shared_ptr<MockSwitch> switcH;
 
-    for (int _=0; _<100; _++)
+    PowerControl* power_control;
+    TemperatureControl* temperature_control;
+};
+
+
+TEST_F(cloud_kochtopf_suite, basic_usage)
+{
+
+    temperature_control->set_target_temperature(91);
+
+    // expecting the switch turned on really soon now
+    int loop;
+    for (loop=0; loop<100; loop++) {
+        if (switcH->get_state() == true)
+            break;
         std::this_thread::sleep_for(1ms);
+    }
+    ASSERT_TRUE(switcH->get_state());
+    ASSERT_NE(loop, 100);
 
+    sensor->set_temperature(95);  // target temperature reached
+
+    // expecting the switch turned off really soon now
+    for (loop=0; loop<100; loop++) {
+        if (switcH->get_state() == false)
+            break;
+        std::this_thread::sleep_for(1ms);
+    }
     ASSERT_FALSE(switcH->get_state());
-
-    pc->stop();
+    ASSERT_NE(loop, 100);
 }
